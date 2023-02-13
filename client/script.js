564c8b3ed65c3eeab0ac41f42337ef42c9fe480f
iwaitu <iwaitu@aliyun.com>
2023-02-13 16:08:09 +0800

    update client code

--- client/script.js
+++ client/script.js
@@ -11,10 +11,8 @@ function loader(element) {
     element.textContent = ''
 
     loadInterval = setInterval(() => {
-        // Update the text content of the loading indicator
         element.textContent += '.';
 
-        // If the loading indicator has reached three dots, reset it
         if (element.textContent === '....') {
             element.textContent = '';
         }
@@ -34,9 +32,6 @@ function typeText(element, text) {
     }, 20)
 }
 
-// generate unique ID for each message div of bot
-// necessary for typing text effect for that specific reply
-// without unique ID, typing text will work on every element
 function generateUniqueId() {
     const timestamp = Date.now();
     const randomNumber = Math.random();
@@ -87,10 +82,19 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const userId = localStorage.getItem("id_token");;
+    let userId = '';
+    const storeItem = localStorage.getItem("oidc.user:https://sts.ivilson.com:assistant");
+    if(storeItem) {
+        userId = JSON.parse(storeItem).profile.preferred_username
+    }
+    if(userId == '') {
+        userId = 'iwaitu';
+    }
+    
 
-    const response = await fetch('https://ivilsongpt.onrender.com', {
+    const response = await fetch('http://localhost:5000/', {
         method: 'POST',
+        // mode: 'no-cors',
         headers: {
             'Content-Type': 'application/json',
         },
@@ -104,10 +108,8 @@ const handleSubmit = async (e) => {
 
     if (response.ok) {
         const data = await response.json();
-        const parsedData = data.bot // trims any trailing spaces/'\n' 
-        let htmlData = marked(parsedData);
-        // var temp = new DOMParser().parseFromString(htmlData, "text/xml");
-        // htmlData = temp.firstChild.innerHTML;
+        const parsedData = data.bot;
+        let htmlData = marked(parsedData).replace(/&#39;/g,"'");
         const codeBlocks = htmlData.match(/<pre><code class="[^"]+">[\s\S]+?<\/code><\/pre>/g);
         if (codeBlocks) {
             codeBlocks.forEach(codeBlock => {
@@ -120,7 +122,7 @@ const handleSubmit = async (e) => {
         }
         
         messageDiv.innerHTML = htmlData;
-        // typeText(messageDiv, parsedData)
+        // typeText(messageDiv, htmlData)
         
     } else {
         const err = await response.text()
@@ -128,6 +130,27 @@ const handleSubmit = async (e) => {
         messageDiv.innerHTML = "Something went wrong"
         alert(err)
     }
+}
+
+const handleTest = async (e) => {
+    // console.log(userId);
+    // let s = '```javascript\n var a = "123" \n print(a)\n```';
+    // console.log(s);
+    // let htmlData = marked(s);
+    // console.log(htmlData);
+    // htmlData = htmlData.replace(/&#39;/g,"'");
+    // console.log(htmlData);
+    // const codeBlocks = htmlData.match(/<pre><code class="[^"]+">[\s\S]+?<\/code><\/pre>/g);
+    // if (codeBlocks) {
+    //     codeBlocks.forEach(codeBlock => {
+    //         let language = codeBlock.match(/class="([^"]+)"/)[1];
+    //         language = language.replace("language-","");
+    //         const code = codeBlock.replace(/<[^>]+>/g, '');
+    //         const highlightedCode = hljs.highlight(code,{language: language, ignoreIllegals: true}).value;
+    //         htmlData = htmlData.replace(codeBlock, `<div class='markdown-body'><pre><code class="${language}">${highlightedCode}</code></pre></div>`);
+    //     });
+    // }
+    // console.log(htmlData);
 }
 
 form.addEventListener('submit', handleSubmit)
@@ -138,7 +161,10 @@ form.addEventListener('keyup', (e) => {
         const end = this.selectionEnd;
         this.value = this.value.substring(0, start) + "\n" + this.value.substring(end);
         this.selectionStart = this.selectionEnd = start + 1;
+        // handleTest(e);
+        return;
     }
+    
     if (e.keyCode === 13) {
         handleSubmit(e)
     }
